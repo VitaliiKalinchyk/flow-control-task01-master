f0bb231038098dda60df00af600f3ec5be0e69a2
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-04 21:31:58 +0200

    04 11

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -3,11 +3,14 @@ package com.epam.rd.autotasks;
 
 class LoopStatements {
     public static int sumOddDigits(int n) {
-        //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        int sum = 0;
+        while (n > 0) {
+            int x = n % 10;
+            if (x % 2 != 0) {
+                sum += x;
+            }
+            n /=10;
+        }
+        return sum;
     }
-
-
-
-
 }
